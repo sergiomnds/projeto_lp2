8895f8b49fe121ca48d4379a89e0aa80be42c515
gustavo Xavier <guxfarias7@gmail.com>
2022-06-15 10:17:37 -0300

    volta ao padrao
    Padrão...

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("So um teste boy!");
+        System.out.println("Hello world!");
     }
 }
