f6e0873963e88677c03d265e32cc1a4bcf0e8f32
allan frança dutra <allanfd12@gmail.com>
2022-07-03 14:49:37 -0300

    add tratamento Exceção add consulta sobre conteiners ajustes

--- src/Empilhador.java
+++ src/Empilhador.java
@@ -2,25 +2,135 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Empilhador {
-    Map<Character,Pilha> pilhas = new HashMap<>();
-    
-    Empilhador(){
-        for(int i =0; i<12; i++){
-            pilhas.put((char)(i + 65), new Pilha());
+    private Map<Character, Pilha> pilhas = new HashMap<>();
+    private int limiteEmpilhamento=6;
+
+    Empilhador() {
+        for (int i = 0; i < 12; i++) {
+            char letra = (char) (i + 65);
+            pilhas.put(letra, new Pilha(letra,limiteEmpilhamento));
         }
     }
-    public boolean empilhar(char posicao, Conteiner conteiner){
+
+    public Conteiner informacaoConteinerTopo(char posicao) {
+
+        this.throwsIfNotHasPilha(posicao);
+
+        return pilhas.get(posicao).verTopo();
+    }
+
+    public boolean empilhar(char posicao, Conteiner conteiner) throws IllegalArgumentException {
+
+        this.throwsIfNotHasPilha(posicao);
+
         Pilha pilha = pilhas.get(posicao);
         return pilha.empilhar(conteiner);
     }
 
-    public boolean desempilhar( String indentificador){
+    public boolean desempilhar(String indentificador) throws IllegalArgumentException {
         char posicao = indentificador.toUpperCase().charAt(0);
-        if(!pilhas.containsKey(posicao)){
-            System.out.println("Identificador inválido");
-            return false;
+
+        this.throwsIfNotHasPilha(posicao);
+
+        return pilhas.get(posicao).desempilhar(indentificador);
+
+    }
+
+    private void throwsIfNotHasPilha(char posicao) {
+        if (!this.hasPilha(posicao)) {
+            System.out.println("posição inválida");
+            throw new IllegalArgumentException("posição inválida");
         }
-        return pilhas.get(posicao).desempilhar(indentificador) ;
+    }
+
+    public Map<String, Integer> getQuantidadePorTipoCarga() {
+        Map<String, Integer> qauntidadePorTipoCarga = new HashMap<>();
+        for (TipoCarga t : TipoCarga.values()) {
+            int quantidade = 0;
+            for (Pilha pilha : pilhas.values()) {
+                quantidade += pilha.getQuantidadePorTipoCarga(t);
+            }
+            qauntidadePorTipoCarga.put(t.getDescricao(), quantidade);
+        }
+
+        return qauntidadePorTipoCarga;
+    }
+    public Map<String,Integer> getQuantidadePorTipoOperacao(){
+        Map<String,Integer> quantidadePorTipoOperacao = new HashMap<>();
+        for(TipoOperacao t : TipoOperacao.values()){
+            int quantidade = 0;
+            for(Pilha pilha : pilhas.values()){
+                quantidade += pilha.getQuantidadePorTipoOperacao(t);
+            }
+            quantidadePorTipoOperacao.put(t.getDescricao(),quantidade);
+        }
+        return quantidadePorTipoOperacao;
+    }
+    public Map<String,Float> getPesoTotalPorTipoCarga() {
+        Map<String,Float> pesoTotalPorTipoCarga = new HashMap<>();
+        for (TipoCarga t : TipoCarga.values()) {
+            float peso = 0;
+            for (Pilha pilha : pilhas.values()) {
+                peso += pilha.getPesoTotalPorTipoCarga(t);
+            }
+            pesoTotalPorTipoCarga.put(t.getDescricao(), peso);
+        }
+
+        return pesoTotalPorTipoCarga;
+    }
+
+    public String getPilhasVazias(){
+        String pilhasVazias = "";
+        for(Pilha pilha : pilhas.values()){
+            if(pilha.quantidadeConteiners() == 0){
+                pilhasVazias += " "+pilha.getNome();
+            }
+        }
+        return pilhasVazias;
+    }
     
+    public String getGraficoPosicoes(){
+        String grafico = "Relação de posições e seus conteiners:\n\n";
+        Map<Character,Integer> quantidadePorPilha = new HashMap<>();
+
+        for (Pilha pilha : pilhas.values()) {
+            quantidadePorPilha.put(pilha.getNome(), pilha.quantidadeConteiners());
+        }
+        for (int j = limiteEmpilhamento; j >0 ; j--) {
+            for (int i = 0+65; i < 6+65; i++) {
+                char letra = (char) i ;
+                if(quantidadePorPilha.get(letra)>=j){
+                    grafico += "|█|   ";
+                }else{
+                    grafico += "| |   ";
+                }
+            }
+            grafico += "\n";
+        }
+        for (int i = 0+65; i < 6+65; i++) {
+            char letra = (char) i ;
+            grafico +=" "+letra+"    ";
+        }
+        grafico += "\n\n";
+        for (int j = limiteEmpilhamento; j >0 ; j--) {
+            for (int i = 6+65; i < 12+65; i++) {
+                char letra = (char) i ;
+                if(quantidadePorPilha.get(letra)>=j){
+                    grafico += "|█|   ";
+                }else{
+                    grafico += "| |   ";
+                }
+            }
+            grafico += "\n";
+        }
+        for (int i = 6+65; i < 12+65; i++) {
+            char letra = (char) i ;
+            grafico +=" "+letra+"    ";
+        }
+        grafico += "\n\n";
+        return grafico;
+    }
+    public boolean hasPilha(char posicao) {
+        return pilhas.containsKey(posicao);
     }
 }
--- src/Pilha.java
+++ src/Pilha.java
@@ -1,50 +1,125 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class Pilha {
 
-    Stack<Conteiner> pilha= new Stack<>();
+    private Deque<Conteiner> pilha = new ArrayDeque<>();
     private int contador = 1;
-    int limiteConteiners = 6;
+    private int limiteConteiners;
+    private char nome;
 
-    public boolean empilhar(Conteiner conteiner){
+    Pilha(char nome, int limiteConteiners) {
+        this.nome = nome;
+        this.limiteConteiners = limiteConteiners;
+    }
+    public char getNome() {
+        return nome;
+    }
+    public boolean empilhar(Conteiner conteiner) {
 
-        if(pilha.size() < limiteConteiners){
+        if (pilha.size() < limiteConteiners) {
             conteiner.gerarIdentificador(contador);
             pilha.push(conteiner);
             contador++;
             return true;
         }
-            System.out.println("Pilha cheia");
-        
+        System.out.println("Pilha cheia");
+
         return false;
     }
-    
-    public boolean desempilhar(String indentificador){
-        if(pilha.size() == 0){
+
+    public Conteiner verTopo() {
+        if (pilha.isEmpty()) {
+            System.out.println("Pilha vazia");
+            return null;
+        }
+        return pilha.peek();
+    }
+
+    public boolean desempilhar(String indentificador) {
+        if (pilha.isEmpty()) {
             System.out.println("Pilha vazia");
             return false;
         }
 
-        Stack<Conteiner> pilhaAux = new Stack<Conteiner>();
+        Deque<Conteiner> pilhaAux = new ArrayDeque<Conteiner>();
         Boolean encontrou = false;
-        for(int i = 0; i < pilha.size(); i++){
+        for (int i = 0; i < pilha.size(); i++) {
             Conteiner conteiner = pilha.pop();
-            if(conteiner.getIdentificador().equals(indentificador)){
+            if (conteiner.getIdentificador().equals(indentificador)) {
                 encontrou = true;
                 break;
             }
             pilhaAux.push(conteiner);
         }
 
-        for(int i = 0; i < pilhaAux.size(); i++){
+        for (int i = 0; i < pilhaAux.size(); i++) {
             pilha.push(pilhaAux.pop());
         }
 
-        if(!encontrou){
+        if (!encontrou) {
             System.out.println("Conteiner não encontrado");
         }
 
         return encontrou;
-        
+
     }
+
+    public int quantidadeConteiners() {
+        return pilha.size();
+    }
+
+    public int getQuantidadePorTipoCarga(TipoCarga t) {
+        int quantidade = 0;
+
+        Deque<Conteiner> pilhaAux = new ArrayDeque<Conteiner>();
+        for (int i = 0; i < pilha.size(); i++) {
+            Conteiner conteiner = pilha.pop();
+            if (conteiner.getTipoCarga() == t) {
+                quantidade++;
+            }
+            pilhaAux.push(conteiner);
+        }
+        for (int i = 0; i < pilhaAux.size(); i++) {
+            pilha.push(pilhaAux.pop());
+        }
+
+        return quantidade;
+    }
+
+    public int getQuantidadePorTipoOperacao(TipoOperacao t) {
+        int quantidade = 0;
+        Deque<Conteiner> pilhaAux = new ArrayDeque<Conteiner>();
+        for (int i = 0; i < pilha.size(); i++) {
+            Conteiner conteiner = pilha.pop();
+            if (conteiner.getTipoOperacao() == t) {
+                quantidade++;
+            }
+            pilhaAux.push(conteiner);
+        }
+        for (int i = 0; i < pilhaAux.size(); i++) {
+            pilha.push(pilhaAux.pop());
+        }
+        return quantidade;
+    }
+
+    public float getPesoTotalPorTipoCarga(TipoCarga t) {
+        float peso = 0;
+
+        Deque<Conteiner> pilhaAux = new ArrayDeque<Conteiner>();
+        for (int i = 0; i < pilha.size(); i++) {
+            Conteiner conteiner = pilha.pop();
+            if (conteiner.getTipoCarga() == t) {
+                peso += conteiner.getPesoCarga();
+            }
+            pilhaAux.push(conteiner);
+        }
+        for (int i = 0; i < pilhaAux.size(); i++) {
+            pilha.push(pilhaAux.pop());
+        }
+
+        return peso;
+    }
+
 }
